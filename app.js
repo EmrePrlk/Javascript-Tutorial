38c4a0a92e418cd26e44d64c5c4a0a99a5941f6e
EmrePrlk <mr.prlk@gmail.com>
2023-04-26 02:47:20 +0300

    a

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Hello World from JS dersleri");
+
+
+
+
+
---
+++ degisken_sabit_kavramlari.js
@@ -0,0 +1,26 @@
+var yas = 38;
+var isim = "Emre";
+
+
+console.log(yas);
+
+
+let tckn = 12345678910;
+let sayı1 = 123123123;
+let sayı2 = 1233211233213;
+let sayı3 = 90654975067034875;
+let benimDogumYilim = 1675;
+
+let sayı5;
+sayı5 = 50
+sayı5 = 60
+sayı5 = 100
+console.log(sayı5)
+
+const piSayisi = 3.14;
+
+console.log(piSayisi)
+
+note = 50;  // bu şekilde değişken tanımlanabilir. fakat yine de var,let,const kullanılmalı
+console.log(note)
+
