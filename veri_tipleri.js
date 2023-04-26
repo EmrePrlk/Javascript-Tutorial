11f49a117aae92a1ae3e30fa7c98bcc66556cb8a
EmrePrlk <mr.prlk@gmail.com>
2023-04-26 03:07:46 +0300

    a

---
+++ veri_tipleri.js
@@ -0,0 +1,50 @@
+let sayi = 50;
+console.log(sayi);  
+console.log(typeof sayi);
+
+let ondalik = 3.14
+
+console.log(ondalik, typeof ondalik);
+
+let isim = "Furkan"
+
+console.log(isim, typeof isim);
+
+let soyisim = 'Kaya';
+
+let cumle = "emre'nin bilgisayarı"
+let cumle1 = 'emre\'nin bilgisayarı'
+
+console.log(cumle, cumle1, typeof cumle);
+
+
+let tamIsim = isim + " " + soyisim;
+
+console.log(tamIsim, typeof tamIsim);
+
+//template literal - backtick
+
+let tamIsim2 = `${isim} ${soyisim}`;
+console.log("template literal", tamIsim2, typeof tamIsim2);
+console.log(`template literal ile yazdır ${tamIsim2} ${sayi} ${ondalik} ${cumle} ${cumle1} ${tamIsim2} ${tamIsim2} ${tamIsim2} ${tamIsim2} ${tamIsim2}`)
+
+
+
+let asalSayiMi = true;
+console.log("veri türümüzün değeri " + asalSayiMi + " veri tipi:" + typeof asalSayiMi)
+
+
+let renk;
+
+renk = ("yeşil");
+console.log(renk, typeof renk)
+
+
+
+
+let yas = null;
+console.log(yas, typeof yas);   
+
+
+
+
