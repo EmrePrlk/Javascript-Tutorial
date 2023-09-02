a85471eec6a289fc2933267a5b5172e229c85dbe
EmrePrlk <mr.prlk@gmail.com>
2023-09-02 19:06:15 +0300

    add code-challanges

---
+++ code-challenges.js
@@ -0,0 +1,27 @@
+// challenge 1
+
+// BMI = mass / height ** 2
+
+const massMark = 78
+const heightMark = 1.69
+const massJohn = 92
+const heightJohn = 1.95
+
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / heightJohn ** 2;
+
+markHigherBMI = BMIMark > BMIJohn
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// const massMark = 78
+// const heightMark = 1.69
+// const massJohn = 92
+// const heightJohn = 1.95
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn
+
+// console.log(BMIMark, BMIJohn, markHigherBMI);
--- script-3-basicOperators.js
+++ script-3-basicOperators.js
@@ -1,1 +1,34 @@
-const ageJonas = 20;
+const now = 2037
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+
+const firstName = 'Jonas';
+const lastName = 'bosh';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; //x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison Operator
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+
+console.log(ageSarah >= 18);
+
+
+const isFullAge = ageSarah >= 18;
+
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+
