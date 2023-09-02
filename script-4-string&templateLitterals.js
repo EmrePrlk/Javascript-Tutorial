60668b52f604e9a1b20482b019d188e9211c64b1
EmrePrlk <mr.prlk@gmail.com>
2023-09-02 23:17:45 +0300

    4

---
+++ script-4-string&templateLitterals.js
@@ -0,0 +1,18 @@
+const firstName = 'jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2023;
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`
+console.log(jonasNew);
+console.log(`Just a reguar string`);
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
