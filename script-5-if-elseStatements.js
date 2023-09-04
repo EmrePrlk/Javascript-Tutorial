c2cdfb56c696267e208affda0006a4ef2e70a523
EmrePrlk <mr.prlk@gmail.com>
2023-09-04 17:19:40 +0300

    add lecture 5

---
+++ script-5-if-elseStatements.js
@@ -0,0 +1,9 @@
+const age = 16
+
+if (age >= 18) {
+    console.log('Sarah can start driving.');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is not old enough, please wait ${yearsLeft} years to have a license`);
+}
+
